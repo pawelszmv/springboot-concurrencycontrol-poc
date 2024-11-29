ad05a1b15b9ca6ef3c9999b6cf106d8b793fd085
Saad Meddiche <s.meddiche@itroadgroup.com>
2024-11-29 10:45:57 +0100

    add all urls to EndPointTest

--- src/main/java/com/concurrencycontrol/demo/EndPointTest.java
+++ src/main/java/com/concurrencycontrol/demo/EndPointTest.java
@@ -10,9 +10,20 @@ import java.util.concurrent.Executors;
 @Slf4j
 public class EndPointTest {
 
-    private static final String URL = "http://localhost:8080/super-products/buy?id=1&quantity=1";
+    // Normal -> No Locking applied
+    private static final String URL = "http://localhost:8080/products/buy-synchronized?id=1&quantity=1";
 
-    private static final int NUM_THREADS = 25;
+    // Memory level -> Pessimistic Locking
+    // private static final String URL = "http://localhost:8080/products/buy-normal?id=1&quantity=1";
+
+    // Database level -> Pessimistic Locking
+    // private static final String URL = "http://localhost:8080/super-products/buy?id=1&quantity=1";
+
+    // Database level -> Optimistic Locking
+    // private static final String URL = "http://localhost:8080/ultra-products/buy?id=1&quantity=1";
+
+
+    private static final int NUM_THREADS = 50;
 
     public static void main(String[] args) {
         ExecutorService executorService = Executors.newFixedThreadPool(NUM_THREADS);
